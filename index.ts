9595efc89e8cebf2082df6775fdfb961fa7b466b
tuyenmon2201 <letuyen22012202@gmail.com>
2024-09-15 20:27:51 +0700

    Create route basic

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("OK");
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express();
+const port: number = 3003;
+
+app.get("/topics", (req: Request, res: Response) => {
+    res.send("Chủ đề bài hát!");
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
