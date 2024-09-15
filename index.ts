68fbe81235404d320b98750ea10eaa790fc445d1
tuyenmon2201 <letuyen22012202@gmail.com>
2024-09-15 20:44:07 +0700

    Connect database

--- index.ts
+++ index.ts
@@ -1,10 +1,15 @@
 import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app: Express = express();
-const port: number = 3003;
+const port: number | string = process.env.PORT || 3003;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
+
+import { connectDatabase } from "./config/database";
+connectDatabase();
 
 app.get("/topics", (req: Request, res: Response) => {
     res.render("client/pages/topics/index");
