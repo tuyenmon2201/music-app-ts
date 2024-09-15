f0891837993b8ffab57b926d70caf0ff6edbaacd
tuyenmon2201 <letuyen22012202@gmail.com>
2024-09-15 20:51:57 +0700

    Get music topic

--- index.ts
+++ index.ts
@@ -5,13 +5,21 @@ dotenv.config();
 const app: Express = express();
 const port: number | string = process.env.PORT || 3003;
 
+import Topic from "./models/topic.model";
+
 app.set("views", "./views");
 app.set("view engine", "pug");
 
 import { connectDatabase } from "./config/database";
 connectDatabase();
 
-app.get("/topics", (req: Request, res: Response) => {
+app.get("/topics", async (req: Request, res: Response) => {
+    const topics = await Topic.find({
+        deleted: false
+    });
+
+    console.log(topics);
+    
     res.render("client/pages/topics/index");
 })
 
