3885a41db330e69bf7f70a135921ef0eb24f5828
tuyenmon2201 <letuyen22012202@gmail.com>
2024-09-21 15:25:35 +0700

    Create view for music add page

--- controllers/admin/song.controller.ts
+++ controllers/admin/song.controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import Song from "../../models/song.model";
+import Topic from "../../models/topic.model";
+import Singer from "../../models/singer.model";
 
 export const index = async (req: Request, res: Response) => {
 
@@ -10,5 +12,21 @@ export const index = async (req: Request, res: Response) => {
     res.render("admin/pages/songs/index", {
         pageTitle: "Quản lí chủ đề",
         songs: songs
+    });
+}
+
+export const create = async (req: Request, res: Response) => {
+    const topics = await Topic.find({
+        deleted: false
+    }).select("title");
+
+    const singers = await Singer.find({
+        deleted: false
+    }).select("fullName");
+
+    res.render("admin/pages/songs/create", {
+        pageTitle: "Quản lí chủ đề",
+        topics: topics,
+        singers: singers
     });
 }
--- index.ts
+++ index.ts
@@ -15,11 +15,14 @@ app.use(bodyParser.json());
 import { routesClient } from "./routes/client/index.route";
 import { routesAdmin } from "./routes/admin/index.route";
 import { systemConfig } from "./config/system";
+import path from "path";
 
-app.set("views", "./views");
+app.set("views", `${__dirname}/views`);
 app.set("view engine", "pug");
 
 app.locals.prefixAdmin = systemConfig.prefixAdmin;
+
+app.use('/tinymce', express.static(path.join(__dirname, 'node_modules', 'tinymce')));
 
 routesAdmin(app);
 
